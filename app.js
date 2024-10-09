b0c20db53a18bcd2dc5ea08de5fda38413469e91
sabriNoor <nooraosama63@gmail.com>
2024-10-09 18:46:09 +0300

    category add delete update list

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const authRouter = require('./routers/auth'); // Adjust the path accordingly
 const itemRouter = require('./routers/itemRouter');
+const catRouter = require('./routers/categoryRouter');
+
 
 
 const cookieParser = require('cookie-parser');
@@ -12,7 +14,7 @@ app.use(cookieParser());
 
 app.use('/', authRouter); // Use the auth router
 app.use('/item',itemRouter);
-
+app.use('/cat',catRouter);
 
 app.listen(5000, () => {
     console.log('Server running on http://localhost:5000');
--- models/category.js
+++ models/category.js
@@ -23,4 +23,58 @@ const findCategoryById = async (id) => {
     return results;
 
 }
-module.exports = {category, findCategoryById};
+
+const catAdd=async(catName)=>{
+
+    const sqlQuery = `INSERT INTO Categories (catName) VALUES ( :catName )`;
+
+const results = await sequelize.query(sqlQuery, {
+replacements: {catName},
+type: QueryTypes.INSERT
+})
+
+return results;
+}
+
+const catUpdate=async(id,name)=>{
+
+    const sqlQuery = `
+    UPDATE categories 
+    SET catName =:name
+    WHERE catId = :id
+`;
+
+await sequelize.query(sqlQuery, {
+    replacements: { name: name, id: id }, // Use 'id' instead of 'catId'
+    type: QueryTypes.UPDATE
+});
+
+}
+
+const catDelete=async(id)=>{
+
+    const sqlQuery = `
+    DELETE FROM categories 
+    WHERE catId=:id`;
+   
+await sequelize.query(sqlQuery, {
+    replacements: { id: id }, // Use 'id' instead of 'catId'
+    type: QueryTypes.UPDATE
+});
+
+}
+
+const catList=async()=>{
+
+    const sqlQuery = `SELECT * from categories `;
+
+    const result = await sequelize.query(sqlQuery, {
+        type: QueryTypes.SELECT,
+    });
+    console.log("Query result:", result);
+
+    return result;
+
+}
+
+module.exports = {category, findCategoryById,catAdd,catUpdate,catDelete,catList};
